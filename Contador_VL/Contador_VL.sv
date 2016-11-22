df7cc8d954afde736357237d50ee8f50a78d01e5
victtor <vhugoloureiro@outlook.com>
2016-11-22 14:59:01 -0300

    Correções feitas na questão 1

--- Contador_VL/Contador_VL.sv
+++ Contador_VL/Contador_VL.sv
@@ -1,20 +1,20 @@
-module Contador_VL(input logic reset, clock,
+module Contador_VL(input logic CLEAR, CP,
 							output logic [3:0]out);
 							logic cont;							
-always_ff @(posedge clock or posedge reset)
+always_ff @(posedge CP or posedge CLEAR)
 
 	begin	
-	if (reset)
+	if (CLEAR)
 		begin
 		cont <= 0;
 		out <= 4'd0;
 		end	
 	else		
 		if(cont == 0)
-			if(out == 4'd14)
+			if(out == 4'd15)
 				begin
 				cont <= 1;
-				out <= out + 4'd1;			
+				out <= out - 4'd1;			
 				end
 			else
 				out <= out + 4'd1;
